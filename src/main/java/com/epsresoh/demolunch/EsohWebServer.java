25ba8597b0657d6b0a945836d07f9bdfba91eac1
Alexandru Muntean <alexandru.muntean@e-spres-oh.com>
2016-11-22 18:31:45 +0200

    Step 4. Add response length

--- src/main/java/com/epsresoh/demolunch/EsohWebServer.java
+++ src/main/java/com/epsresoh/demolunch/EsohWebServer.java
@@ -28,6 +28,8 @@ public class EsohWebServer {
 			System.out.println(clientText);
 		}
 
+		String response = getResponse();
+
 		PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
 		writer.println("HTTP/1.1 200 OK");
 		writer.println("Date: Tue, 22 Nov 2016 16:21:12 GMT");
@@ -35,20 +37,21 @@ public class EsohWebServer {
 		writer.println("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
 		writer.println("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
 		writer.println("Pragma: no-cache");
-		writer.println("Content-Length: 7713");
+		writer.println("Content-Length: " + response.getBytes().length);
 		writer.println("X-Powered-By: PleskLin");
 		writer.println("Connection: close");
 		writer.println("Content-Type: text/html; charset=UTF-8");
 		writer.println("");
-		writer.println("<html>hello world</html>");
-		writer.println("");
+		writer.println(response);
 		writer.flush();
-		
-		Thread.sleep(1000);
-		
+
 		clientSocket.close();
 		serverSocket.close();
 
 		System.out.println("We're closed! Bye!");
+	}
+
+	private String getResponse() {
+		return "<html>hello world</html>";
 	}
 }
